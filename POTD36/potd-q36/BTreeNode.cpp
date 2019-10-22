88a42fe67e4af3eefcc466abc27132781a321bc7
Nathan <nathan@Nathans-MacBook-Pro.local>
2019-10-22 11:45:24 -0500

    Solved POTD36

---
+++ POTD36/potd-q36/BTreeNode.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include "BTreeNode.h"
+
+
+void _traverse(BTreeNode* root, std::vector<int>& vec) {
+    if (root == NULL) return;
+    
+    for (unsigned i = 0; i < root->elements_.size(); i++) {
+        if (!root->is_leaf_) {
+            _traverse(root->children_[i], vec);
+        }
+
+        vec.push_back(root->elements_[i]);
+    }
+
+    if (!root->is_leaf_) {
+        _traverse(root->children_[root->children_.size() - 1], vec);
+    }
+}
+
+std::vector<int> traverse(BTreeNode* root) {
+    // your code here
+    std::vector<int> v;
+    _traverse(root, v);
+    return v;
+}
